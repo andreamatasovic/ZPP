de33d3634dfa53a38e08a49fea82a354883f274e
andreamatasovic <117307621+andreamatasovic@users.noreply.github.com>
2024-09-03 18:20:11 +0200

    Delete CRP directory

--- CRP/Container.py
+++
@@ -1,9 +0,0 @@
-class Container:
-    def __init__(self, container_id, position, reshuffle_index, lookahead_cost=0):
-        self.id = container_id
-        self.position = position
-        self.reshuffle_index = reshuffle_index
-        self.lookahead_cost = lookahead_cost
-
-    def __repr__(self):
-        return f"Container(id={self.id}, position={self.position}, reshuffle_index={self.reshuffle_index}, lookahead_cost={self.lookahead_cost})"
